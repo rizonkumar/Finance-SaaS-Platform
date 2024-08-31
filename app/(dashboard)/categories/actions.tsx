20e23cbf224c0c35a3def9e4744a2b14922da108
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-08-31 17:55:25 +0530

    Feat: Inital commmit for categories

---
+++ app/(dashboard)/categories/actions.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Trash2, Edit, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+
+import { useOpenAccount } from "@/features/hooks/use-open-account";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useConfirm } from "@/hooks/use-confirm";
+
+type Props = {
+  id: string;
+};
+
+export const Actions = ({ id }: Props) => {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this transaction."
+  );
+
+  const deleteMutation = useDeleteAccount(id);
+  const { onOpen } = useOpenAccount();
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      deleteMutation.mutate();
+    }
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}
+          >
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}
+          >
+            <Trash2 className="size-4 mr-2" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};

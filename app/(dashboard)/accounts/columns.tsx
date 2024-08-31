c432537769e64dbb64d8c1efdb844f8632af9863
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-08-31 16:40:39 +0530

    Feat: Implemented Edit Option along along with the edit API

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -8,6 +8,7 @@ import { ArrowUpDown } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Actions } from "./actions";
 
 export type ResponseType = InferResponseType<
   typeof client.api.accounts.$get,
@@ -50,5 +51,9 @@ export const columns: ColumnDef<ResponseType>[] = [
         </Button>
       );
     },
+  },
+  {
+    accessorKey: "Actions",
+    cell: ({ row }) => <Actions id={row.original.id} />,
   },
 ];

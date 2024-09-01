8409d62da40afbcb097741e32d2cfccdf27ded60
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-09-01 21:10:29 +0530

    Fix: Few fix

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -38,7 +38,6 @@ const TransactionsPage = () => {
   const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
 
   const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
-    console.log({ results });
     setImportResults(results);
     setVariant(VARIANTS.IMPORT);
   };

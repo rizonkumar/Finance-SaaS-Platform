56580df526a9d73ff00cb3909b9c59bba7685160
rizonkumar <rizon.kumar.rahi@gmail.com>
2026-08-19 10:08:55 +0530

    Update button component: Adjust gap between items in button variants for improved layout consistency.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-x-2 whitespace-nowrap rounded-sm text-sm font-medium transition-colors disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-700 [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-x-1.5 whitespace-nowrap rounded-sm text-sm font-medium transition-colors disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-700 [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
